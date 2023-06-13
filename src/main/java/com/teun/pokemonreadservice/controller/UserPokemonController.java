46c0503b43e884c9f1cedb6a8d5959d316b756d5
Teun <teunbuis17@gmail.com>
2023-06-13 21:21:54 +0200

    Update UserPokemonController.java

--- src/main/java/com/teun/pokemonreadservice/controller/UserPokemonController.java
+++ src/main/java/com/teun/pokemonreadservice/controller/UserPokemonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/userpokemon")
+@RequestMapping("api/userpokemonread")
 public class UserPokemonController {
     @Autowired
     UserPokemonService service;
