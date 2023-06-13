56f9eafa0072fbc4f65a9a083dd985cf513b3567
Teun <teunbuis17@gmail.com>
2023-06-13 14:13:39 +0200

    Init

---
+++ src/main/java/com/teun/pokemonreadservice/PokemonreadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teun.pokemonreadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonreadServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonreadServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teun/pokemonreadservice/PokemonreadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teun.pokemonreadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonreadServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
