26cbfcd85d44f8b7a6801369c83cc79bf9c6c167
Teun <teunbuis17@gmail.com>
2023-06-13 21:16:15 +0200

    Update PokemonDTO.java

--- src/main/java/com/teun/pokemonreadservice/dto/PokemonDTO.java
+++ src/main/java/com/teun/pokemonreadservice/dto/PokemonDTO.java
@@ -3,7 +3,9 @@ package com.teun.pokemonreadservice.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.io.Serializable;
 
@@ -14,16 +16,18 @@ import java.io.Serializable;
 public class PokemonDTO implements Serializable {
 
 
-    private Long id;
+    @Id
+    @MongoId
+    private String id;
     private Long dexNumber;
     private String name;
     private Boolean legendary;
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
